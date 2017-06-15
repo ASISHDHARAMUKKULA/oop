20d64e83066994d8086e2196aeac6128f15c5fcc
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2017-06-15 18:07:06 +0530

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,22 @@
+// first program in c++
+
+
+#include<iostream>
+using namespace std
+int main()
+{
+    cout<<"hello world";
+    return 0;
+
+}
+
+
+output:hello world
+
+// cout is used for printing on console
+
+//C++ offers various headers, each of which contains information needed for programs to work properly.
+This particular program calls for the header <iostream>.
+The number sign (#) at the beginning of a line targets the compiler's pre-processor. 
+In this case, #include tells the pre-processor to include the <iostream> header.
+The <iostream> header defines the standard stream objects that input and output data.//
