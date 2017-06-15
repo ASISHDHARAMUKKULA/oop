d08e906d42850b72142a52e5b8b1b80e127cd753
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2017-06-15 19:31:37 +0530

    Add files via upload

---
+++ newline.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std
+int main()
+{
+    cout<<"hi"<<endl;
+    cout<<"Asish"<<endl;
+    return 0;
+
+}
+// Alternative for newline (endl) is "\n"
+#include<iostream>
+using namespace std
+int main()
+{
+    cout<<"hi\n";
+    cout<<"Asish\n";
+    return 0;
+
+}
+
