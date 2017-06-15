60f76eab0e34bb345f7b09b9fc046d8e6ada16f7
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2017-06-15 22:46:37 +0530

    Add files via upload

---
+++ basicarithmeticoperations.cpp
@@ -0,0 +1,52 @@
+Addition Operation
+
+
+//The addition operator adds its operands together.
+int x = 40 + 60;
+cout << x;
+
+// Outputs 100
+
+Subtraction Operation
+
+
+//The subtraction operator subtracts one operand from the other.
+int x = 100 - 60;
+cout << x;
+
+//Outputs 40
+
+Multiplication Operation
+
+
+//The multiplication operator multiplies its operands.
+
+int x = 5 * 6;
+cout << x;
+
+//Outputs 30
+
+
+Division Operation
+
+
+//The division operator divides the first operand by the second.
+//Any remainder is dropped in order to return an integer value.
+
+
+int x = 10 / 3;
+cout << x;
+
+// Outputs 3
+
+
+Modular Division
+
+//The modulus operator (%) is informally known as the remainder operator because it returns the remainder after an integer division.
+
+
+
+int x = 25 % 7;
+cout << x;
+
+// Outputs 4
